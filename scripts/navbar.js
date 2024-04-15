6444749c6430170a40f762efcd8fe25c9e766ca3
Ashik E Elahi <s2210724113@ru.ac.bd>
2024-04-15 18:04:44 +0600

    Some correction

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -11,6 +11,21 @@ function activeLink() {
     list.forEach((item, index) => {
         if(item.classList[1] == "active") {
             pages[index].classList.add('active_page')
+            if(index == 1) {
+                let order = document.getElementById('order')
+                let iframe = document.createElement('iframe')
+                iframe.src = "./pages/distribution_and_order_statistics.html"
+                iframe.id = "iOrder"
+                iframe.classList = "iframe"
+                order.appendChild(iframe)
+            } else if(index == 2) {
+                let R = document.getElementById('r')
+                let iframe = document.createElement('iframe')
+                iframe.src = "./pages/Practical_Session_5_R.html"
+                iframe.id = "iR"
+                iframe.classList = "iframe"
+                R.appendChild(iframe)
+            }
         } else {
             pages[index].classList.remove('active_page')
         }
