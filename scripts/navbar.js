4770a4ebe7a34ce5af261a93a230c95c40061c8e
ashikeelahi3 <ashikeelahi3@gmail.com>
2024-04-15 23:06:36 +0600

    Convert home into iframe

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -12,21 +12,25 @@ function activeLink() {
         if(item.classList[1] == "active") {
             pages[index].classList.add('active_page')
             if(index == 1) {
-                let order = document.getElementById('order')
-                order.removeChild(order.firstElementChild);
-                let iframe = document.createElement('iframe')
-                iframe.src = "./pages/distribution_and_order_statistics.html"
-                iframe.id = "iOrder"
-                iframe.classList = "iframe"
-                order.appendChild(iframe)
+                setTimeout(() => {
+                    let order = document.getElementById('order')
+                    order.removeChild(order.firstElementChild);
+                    let iframe = document.createElement('iframe')
+                    iframe.src = "./pages/distribution_and_order_statistics.html"
+                    iframe.id = "iOrder"
+                    iframe.classList = "iframe"
+                    order.appendChild(iframe)
+                }, 3000)
             } else if(index == 2) {
-                let R = document.getElementById('r')
-                R.removeChild(R.firstElementChild);
-                let iframe = document.createElement('iframe')
-                iframe.src = "./pages/Practical_Session_5_R.html"
-                iframe.id = "iR"
-                iframe.classList = "iframe"
-                R.appendChild(iframe)
+                setTimeout(() => {
+                    let R = document.getElementById('r')
+                    R.removeChild(R.firstElementChild);
+                    let iframe = document.createElement('iframe')
+                    iframe.src = "./pages/Practical_Session_5_R.html"
+                    iframe.id = "iR"
+                    iframe.classList = "iframe"
+                    R.appendChild(iframe)
+                })
             }
         } else {
             pages[index].classList.remove('active_page')
